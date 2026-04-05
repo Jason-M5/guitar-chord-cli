ee2b9e0467c24560238d10421191ce6752dede59
Your Name <youremail@example.com>
2026-04-05 10:53:32 -0700

    attempting to structure

---
+++ chords.py
@@ -0,0 +1,12 @@
+CHOROMATIC_SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
+
+def get_chord_notes(chord):
+    #return the notes of the chord
+    pass
+
+for i in MAJOR_SCALE:
+    MAJOR_SCALE_NOTES = []
+    MAJOR_SCALE_NOTES.append(CHOROMATIC_SCALE[i])
+    print(MAJOR_SCALE_NOTES)
---
+++ fretboard.py
@@ -0,0 +1,4 @@
+def make_fretboard(tuning=['E', 'A', 'D', 'G', 'B', 'E']):
+    #make a fretboard for the tuning
+    pass
+
