923de8673b7f953f7591ec6d546ce15591a81308
Your Name <youremail@example.com>
2026-04-07 16:20:18 -0700

    added frets with 0's

--- chords.py
+++ chords.py
@@ -5,18 +5,18 @@ MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
 MAJOR_SCALE_NOTES = []
 
 #this returns a major scale for a key.  TODO: make it a function
+def get_major_scale():
+    key = input("Enter the key: ").upper()
 
-key = input("Enter the key: ").upper()
+    if key in CHOROMATIC_SCALE:
+        key_index = CHOROMATIC_SCALE.index(key)
 
-if key in CHOROMATIC_SCALE:
-    key_index = CHOROMATIC_SCALE.index(key)
+    else:
+        print("Invalid key")
+        exit()
 
-else:
-    print("Invalid key")
-    exit()
+    for i in MAJOR_SCALE:
+        i += key_index
+        MAJOR_SCALE_NOTES.append(CHOROMATIC_SCALE[i % len(CHOROMATIC_SCALE)])
 
-for i in MAJOR_SCALE:
-    i += key_index
-    MAJOR_SCALE_NOTES.append(CHOROMATIC_SCALE[i % len(CHOROMATIC_SCALE)])
-
-print(MAJOR_SCALE_NOTES)
+    print(MAJOR_SCALE_NOTES)
--- fretboard.py
+++ fretboard.py
@@ -1,6 +1,14 @@
-def make_fretboard(tuning=['E', 'A', 'D', 'G', 'B', 'E']):
+from calendar import c
+from chords import get_major_scale
+from chords import CHOROMATIC_SCALE
+
+def make_fretboard(tuning=[CHOROMATIC_SCALE[7], CHOROMATIC_SCALE[0], CHOROMATIC_SCALE[5], CHOROMATIC_SCALE[10], CHOROMATIC_SCALE[2], CHOROMATIC_SCALE[7]]):
+    fretboard = []
+    major_scale = get_major_scale()
     fret = "---|"
+    fret_note = "-O-|"
     for string in tuning:
-        print(f"|{string}| |{fret * 22}")
-    
+        
+        print(f"|{string}| |{fret * 11 + fret_note * 11}")
+
 make_fretboard()
