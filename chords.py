18df547cb089a72421353c0ee11a190a9e6962c5
Your Name <youremail@example.com>
2026-04-09 18:57:05 -0700

    need to think about structure, nothing good yet

--- chords.py
+++ chords.py
@@ -1,15 +1,15 @@
 CHOROMATIC_SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-
+KEY = input("Enter the key: ").upper()
 MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
 
 MAJOR_SCALE_NOTES = []
 
-#this returns a major scale for a key.  TODO: make it a function
-def get_major_scale():
-    key = input("Enter the key: ").upper()
 
-    if key in CHOROMATIC_SCALE:
-        key_index = CHOROMATIC_SCALE.index(key)
+def get_major_scale():
+
+
+    if KEY in CHOROMATIC_SCALE:
+        key_index = CHOROMATIC_SCALE.index(KEY)
 
     else:
         print("Invalid key")
@@ -19,4 +19,5 @@ def get_major_scale():
         i += key_index
         MAJOR_SCALE_NOTES.append(CHOROMATIC_SCALE[i % len(CHOROMATIC_SCALE)])
 
-    print(MAJOR_SCALE_NOTES)
+    return MAJOR_SCALE_NOTES
+
--- fretboard.py
+++ fretboard.py
@@ -1,14 +1,38 @@
-from calendar import c
 from chords import get_major_scale
 from chords import CHOROMATIC_SCALE
 
-def make_fretboard(tuning=[CHOROMATIC_SCALE[7], CHOROMATIC_SCALE[0], CHOROMATIC_SCALE[5], CHOROMATIC_SCALE[10], CHOROMATIC_SCALE[2], CHOROMATIC_SCALE[7]]):
-    fretboard = []
-    major_scale = get_major_scale()
+def make_fretboard(tuning=[
+    CHOROMATIC_SCALE[7], #standard tuning...
+    CHOROMATIC_SCALE[0], 
+    CHOROMATIC_SCALE[5], 
+    CHOROMATIC_SCALE[10], 
+    CHOROMATIC_SCALE[2], 
+    CHOROMATIC_SCALE[7]
+    ]):
+    
+    fretboard = {i for i in tuning} #each string in the tuning
+    major_scale = get_major_scale() #returns a list of each note in major scale
+    num_frets = 22 #standard fretboard for now.  TODO: go to 12th and loop to one with a num_frets variable
     fret = "---|"
     fret_note = "-O-|"
+
     for string in tuning:
         
-        print(f"|{string}| |{fret * 11 + fret_note * 11}")
+        print(f"|{string}", end="")
 
+        if string in major_scale:
+            print("|O|", end="")
+        else:
+            print("| |", end="")
+
+
+        for f in range(num_frets):
+            if CHOROMATIC_SCALE.index(string) in major_scale:
+                print(fret, end="")
+            else:
+                print(fret_note, end="")
+            
+        print()    
+            #print(f"|{string}| |{(fret + fret_note) * 11}")
+    print(fretboard)
 make_fretboard()
