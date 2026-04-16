f5a1238aa871a6974790348cad894051446386d8
Your Name <youremail@example.com>
2026-04-15 18:58:08 -0700

    start again

--- fretboard.py
+++ fretboard.py
@@ -1,3 +1,9 @@
+def make_fretboard(tuning=("E", "A", "D", "G", "B", "E"), num_frets=22):
+    
+    num_frets += 1
+    fretboard = {i for i in range(num_frets)}
 
-def make_fretboard()
+    for string in tuning:
+        pass
+
     pass
