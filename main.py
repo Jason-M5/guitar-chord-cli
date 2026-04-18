8769647c1b01d18c790bf66285a5f4ec20e8e27a
Your Name <youremail@example.com>
2026-04-18 11:11:52 -0700

    thinking

---
+++ main.py
@@ -0,0 +1,9 @@
+from notes import Notes
+from fretboard import make_fretboard
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- notes.py
+++ notes.py
@@ -1,23 +1,12 @@
-CHOROMATIC_SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-KEY = input("Enter the key: ").upper()
-MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
+NOTES = ('A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#')
+key = input("Enter the key: ").upper()
+MAJOR_SCALE = [2, 2, 1, 2, 2, 2]
 
 MAJOR_SCALE_NOTES = []
 
 
-def get_major_scale():
 
-
-    if KEY in CHOROMATIC_SCALE:
-        key_index = CHOROMATIC_SCALE.index(KEY)
-
-    else:
-        print("Invalid key")
-        exit()
-
-    for i in MAJOR_SCALE:
-        i += key_index
-        MAJOR_SCALE_NOTES.append(CHOROMATIC_SCALE[i % len(CHOROMATIC_SCALE)])
-
-    return MAJOR_SCALE_NOTES
+def get_major_scale(string, num_frets):
+    #return a tuple with len num frets 1 if in scal 0 if not. starting with string open note
+    pass
 
